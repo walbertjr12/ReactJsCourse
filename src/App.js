942b375a7effd17a6407fefb5c2abe6574c1e7b5
WalbertJr <54550066+WalbertJr@users.noreply.github.com>
2020-07-01 22:06:44 -0600

    SEC 7 Terminada

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 import logo from './logo.svg';
 import './App.css';
 import ConditionalSection from './sections/conditional';
@@ -89,6 +90,42 @@ class CarItem extends Component{
   }
 }
 
+class Box extends Component{
+  render(){
+    return (
+      <div style={{border:'1px solid #000', margin:5, padding: 5}}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class Article extends Component {
+  static propTypes = {
+    author: PropTypes.string.isRequired
+  }
+  /*constructor(props){
+    super(props)
+    if(typeof props.author == 'undefined'){
+      console.warn('author prop is required')
+      throw new Error('author prop is required')
+    }
+  }*/
+  render() {
+    const {author, children, date, title} = this.props
+    return (
+      <section>
+        <h2>{title}</h2>
+        {author && <p><em>Escrito por {author}</em></p>}
+        <Box>{date}</Box>
+        <article>
+          {children}
+        </article>
+      </section>
+    )
+  }
+}
+
 class App extends Component {
   constructor(){
     super()
@@ -113,6 +150,13 @@ class App extends Component {
     const numbers = [1,1,3,4,5];
     return (
       <div>
+        <Article 
+          
+          date = {new Date().toLocaleDateString()}
+          title='Articulo sobre la prop children'>
+            <p>El contenido que envolvemos dentro del componente Article será enviado al componente this.props.children.</p>
+            <strong>Y mantiene las etiquetas y componentes que hayas añadido dentro.</strong>
+        </Article>
         <Forms/>
         <div>
           <h4>Eventos</h4>
